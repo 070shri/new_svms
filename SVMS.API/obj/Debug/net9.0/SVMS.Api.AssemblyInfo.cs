e80e01b0c3535fbe1ec04726e567162bfd3e2db5
Pranav777-del <pranav.m3k@gmail.com>
2026-02-23 22:29:14 +0530

    Added smart-vms-ai as normal folder

--- SVMS.API/obj/Debug/net9.0/SVMS.Api.AssemblyInfo.cs
+++ SVMS.API/obj/Debug/net9.0/SVMS.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SVMS.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82fd6bbe3a1fcc87b60fb92559c94050ff3a42a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed501fd1b5e6fdbe201db661da6097fb506ff40")]
 [assembly: System.Reflection.AssemblyProductAttribute("SVMS.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SVMS.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
